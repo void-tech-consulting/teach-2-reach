cf1357424fe03cd16d56060ff7ffc583c74261e4
Shaan Parikh <pieface3450@0587327472.wireless.umich.net>
2022-01-16 16:00:58 -0500

    Started work on Prospective Students page

--- page-prospective-students.php
+++ page-prospective-students.php
@@ -1,12 +1,21 @@
-<?php 
-  get_header();
-  require get_template_directory() . '/inc/section_vars.php';
+<?php
+get_header();
+require get_template_directory() . '/inc/section_vars.php';
 ?>
 
-<h1>hi</h1>
 
-<?php if (get_theme_mod($prospective_students_img)) { ?>
-    <img src="<?php echo get_theme_mod($prospective_students_img) ?>" class="img" alt="">
+<div class="page">
+  <?php if (get_theme_mod($prospective_students_img)) { ?>
+    <img src="<?php echo get_theme_mod($prospective_students_img) ?>" class="prospective-student-img" alt="">
   <?php } ?>
 
-  <p>why isn't this working</p>
+  <div class="prospective-student-body">
+    <h1 class="prospective-student-h1">Our Programs</h1>
+    <br>
+    <p>Something</p>
+    <p>Something</p>
+    <p>Something</p>
+    <p>Something</p>
+    <p>Something</p>
+  </div>
+</div>
