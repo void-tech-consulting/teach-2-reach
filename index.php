6c8253ba6fcc2c9cb0c542f5c959b12e5e5b058b
mjaosh <mjaosh@umich.edu>
2022-02-06 14:24:05 -0500

    welcome + about us

--- index.php
+++ index.php
@@ -2,46 +2,55 @@
   get_header();
   require get_template_directory() . '/inc/section_vars.php';
 ?>
-<!-- <h1>The Front Page</h1>
 
-<?php if (get_theme_mod($home_top_img)) { ?>
-  <img 
-    src="<?php echo get_theme_mod($home_top_img) ?>" 
-    alt=""
-  >
-<?php } ?>
 
-<?php if (get_theme_mod($home_top_desc)) { ?>
-  <h4>
-    <?php echo get_theme_mod($home_top_desc) ?>
-  </h4>
-<?php } ?> -->
 
-<!-- Example with default video if $home_top_vid isn't set -->
-<!-- <iframe 
-  class="yt-vid" 
-  src="<?php echo get_theme_mod($home_top_vid, 'https://www.youtube.com/embed/A0Wyx-OOX4A'); ?>" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-</iframe> -->
 
-<div class="about_us">
-  <h1 class = "our_philosophy" > Our Philosophy </h1>
 
-  <div class = "text" >
-  <p1 class = "description" >Our philosophy at Teach 2 Reach Inc. is 
-    <p2 class = "orange_text_one" > to teach basic cosmetology essentials and barbering skills </p2>
-    and all it entails in a fun, safe and inviting atmosphere. We intend to instruct in a way that is nontraditional through 
-    <p3 class = "orange_text_one" > integrated learning </p3>
-    , which entails integrating social skills with cosmetology essentials and barbering skills.
- <br> <br> We strive to help student learn through 
-<p4 class = "orange_text_one" > guided learning methods </p4>
-  such as group activities and differentiated learning techniques.  We help students understand what it takes to be successful by 
-  <p5 class = "orange_text_one" > setting goals and objectives </p5> 
-   and helping them reach those goal and objectives successfully. </p1>
+<div class = "welcome" >
+<img src="<?php echo get_template_directory_uri();?>/imgs/Home page photo.png" alt="" class = "collage-photo">
 
- <div>
+<div class = "welcome-text" > 
+  <h1 class = "heading"  > Welcome! </h1>
+  <p1 class = "paragraph" > We teach Barber and Barber Instructor courses (1800 hrs/400hrs respectively) as well as  cosmetology essentials and skills with a social twist. Our cosmetology school offers the full 1500hr State of MI course curriculum for licensure. <p1> 
+</div> 
+
+
+</img> 
+
+</div> 
+
+<div class = "images" >
+
+
+<img src="<?php echo get_template_directory_uri();?>/imgs/group pic 1.jpg" alt="" class = "volunteer">
+<img src="<?php echo get_template_directory_uri();?>/imgs/become a student pic.jpeg" alt="" class = "student">
+<img src="<?php echo get_template_directory_uri();?>/imgs/donate pic.jpeg" alt="" class = "donate">
+
+
 </div>
+
+
+
+<div class = "words" > 
+<p1 class = "first_word" > Volunteer </p1> 
+<p2 class = "second_word" > Become a Student </p2> 
+<p3 class = "third_word" > Donate </p3> 
+</div> 
+
+
+<div class = "video">
+
+<iframe width="600" height="400"
+src="https://www.youtube.com/embed/dSkt8Brso5w">
+</iframe>
+
+<p1 class = "difference" > Working to Make a Difference </p1> 
+
+</div>
+
+
+
 
 
 <?php get_footer(); ?>
---
+++ page-about-us.php
@@ -0,0 +1,73 @@
+<?php 
+  get_header();
+  require get_template_directory() . '/inc/section_vars.php';
+?>
+
+
+<div class="about_us">
+  <h1 class = "our_philosophy" > Our Philosophy </h1>
+
+  <div class = "text" >
+  <p1 class = "description" >Our philosophy at Teach 2 Reach Inc. is 
+    <p2 class = "orange_text_one" > to teach basic cosmetology essentials and barbering skills </p2>
+    and all it entails in a fun, safe and inviting atmosphere. We intend to instruct in a way that is nontraditional through 
+    <p3 class = "orange_text_one" > integrated learning </p3>
+    , which entails integrating social skills with cosmetology essentials and barbering skills.
+ <br> <br> We strive to help student learn through 
+<p4 class = "orange_text_one" > guided learning methods </p4>
+  such as group activities and differentiated learning techniques.  We help students understand what it takes to be successful by 
+  <p5 class = "orange_text_one" > setting goals and objectives </p5> 
+   and helping them reach those goal and objectives successfully. </p1>
+
+</div>
+</div>
+</div> 
+</div>
+
+
+
+<div class = "who_we_are" >
+
+  <div class = "text" > 
+  <h1 class = "heading" > Who We Are </h1> 
+  <p1 class = "paragraph" > We are a nonprofit organization teaching cosmetology essentials and barbering like skills with a social twist.  
+    We call it, "Socially Integrated Learning".  Teach 2 Reach is made up of  a team of Michigan State licensed 
+    professional cosmetologist, barbers, and instructors.  We offer our classes for all ages.  </p1> 
+
+</div> 
+
+<div class = "picture" >
+<img src="<?php echo get_template_directory_uri();?>/imgs/Alvin.jpg" alt="" class = "who_are_we_img">
+</div> 
+</div> 
+</div>
+
+
+
+
+<div class = "goals" > 
+
+<img src="<?php echo get_template_directory_uri();?>/imgs/group photo.jpeg" alt="" class = "quotes_img">
+<h1 class = "creative" > Be Creative <br> <br> <br> Learn New Skills <br> <br> <br> Treat Yourself <br> <br> <br> Make New Friends <h1> 
+<img src="<?php echo get_template_directory_uri();?>/imgs/group photo.jpeg" alt="" class = "quotes_img2">
+
+
+</div> 
+
+<div class = "purple_section" >
+  <h1 class = "promise" > Our Promises: </h1> 
+  <p1 class = "paragraph" > Even if there are hindrances as such may exist pertaining to reading comprehension, writing, or math, we patiently strive to help them succeed to overcome those barriers by offering them tutoring or one on one individualized support. <br> <br>
+ 
+ Our licensed professionals are skilled and trained in the area of cosmetology, barbering and are a custom to working with various types of people from all background, ethnic groups, and handicaps. We strive to treat everyone fairly and create an atmosphere that is nondiscriminatory. <br> <br> 
+  
+ At Teach 2 Reach many of the ourclasses are hands on and therefore it is required that learners take on the responsibility to attempt and contribute often to the learning process.  Learners are required to fully engage in the content material of theory and the practical applications as well.  All supplies and materials are provided for the students by Teach 2 Reach Inc. <br> <br>
+  
+ Students gain knowledge through demonstrations and hands on practical application.  The learning that is required is state mandated and learning standards are compliable with many other institutions that offer similar services in the field. </p1> 
+
+
+</div>
+
+
+
+
+<?php get_footer(); ?>
