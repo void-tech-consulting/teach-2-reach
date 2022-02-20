7edfb1a4e33b13f96f6c456d4aad24b6a7af6aff
Marisa Wong <mariwong@umich.edu>
2022-02-20 15:25:59 -0500

    fix merge conflicts and css

--- inc/section_vars.php
+++ inc/section_vars.php
@@ -10,11 +10,9 @@
   $prospective_students_section = 'prospective_students_section';
   $prospective_students_img = 'prospective_students_icon';
   $prospective_students_name = 'prospective_students_name';
-<<<<<<< HEAD
 
   $news_and_events_section = 'news_and_events_section'; 
   $news_and_events_section_repeater = 'news_and_events_section_repeater'; 
-?>
-=======
   $prospective_students_form_shortcode = 'prospective_students_form_shortcode';
->>>>>>> 62fa0e6c561417a931cae986ee393eb86d4059a7
+
+?>
