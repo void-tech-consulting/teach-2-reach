05a4b28a6663df02339a72deaf695880e78f8981
mjaosh <mjaosh@umich.edu>
2022-04-03 15:03:49 -0400

    more customizers

--- inc/section_vars.php
+++ inc/section_vars.php
@@ -10,4 +10,19 @@
   $prospective_students_section = 'prospective_students_section';
   $prospective_students_img = 'prospective_students_icon';
   $prospective_students_name = 'prospective_students_name';
+
+
+  $about_us_panel = 'about_us_panel'; 
+  $about_us_top_section = 'about_us_top_section';
+  $about_us_middle1_section = 'about_us_middle1_section'; 
+  $about_us_middle2_section = 'about_us_middle2_section'; 
+  $about_us_bottom_section = 'about_us_bottom_section'; 
+  $about_us_philosophy_paragraph = 'about_us_philosophy_paragraph';
+  $about_us_middle1_picture = 'about_us_middle1_picture'; 
+  $about_us_whoweare_paragraph = 'about_us_whoweare_paragraph'; 
+  $about_us_middle2_picture1 = 'about_us_middle2_picture1';
+  $about_us_promise_paragraph = 'about_us_promise_paragraph'; 
+
+
+
 ?>
