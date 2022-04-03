609b1fd4226534790b520c88f657d94dd7f4fec1
Shaan Parikh <pieface3450@0587375678.wireless.umich.net>
2022-04-03 15:09:20 -0400

    Started working on this page

---
+++ page-careers.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+require get_template_directory() . '/inc/section_vars.php';
+?>
+
+<div class="careers-orange-half">
+    <p>somethign somethign seomthi</p>
+</div>
+
+
+<?php get_footer(); ?>
